a85a22893e00bdb0c3c078700efebe18fdfd880c
mathjsx <irzalpa23@outlook.com>
2024-12-31 01:29:23 +0700

    666

---
+++ next.config.js
@@ -0,0 +1,7 @@
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export', // Menentukan bahwa hasil build akan diekspor sebagai static site
+};
+
+module.exports = nextConfig;
